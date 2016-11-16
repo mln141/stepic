28f6de30ac8018075570450e7b179ba03fa3c0df
mln141 <ml@mail333.com>
2016-11-16 22:42:16 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf 
+sudo rm /home/etc/nginx/sites-enabled/default
+sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx start
