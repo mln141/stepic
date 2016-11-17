871735d7bb3f79d1482b17e97017df90ace9d3d9
mln141 <ml@mail333.com>
2016-11-17 08:52:23 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
  #environ['QUERY_STRING'].replace("&", "\r\n")
 def hello(environ, start_response):
- data = environ['QUERY_STRING'].replace("&", "\r\n").replace("?", "\r\n" #b"Hello, World!\n"
+ data = environ['QUERY_STRING'].replace("&", "\r\n").replace("?", "\r\n") #b"Hello, World!\n"
  start_response("200 OK", [
   ("Content-Type", "text/plain"),
   ("Content-Length", str(len(data)))
