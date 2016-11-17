276e31ecd17d1e45274b803cf0cfa6d52eafa17a
mln141 <ml@mail333.com>
2016-11-17 08:24:51 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
  #environ['QUERY_STRING'].replace("&", "\r\n")
-def wsgi_application(environ, start_response):
+def hello(environ, start_response):
     # бизнес-логика
     status = '200 OK'
     headers = [
@@ -8,3 +8,10 @@ def wsgi_application(environ, start_response):
     body = 'Hello, world!'
     start_response(status, headers )
     return [ body ]
+def app(environ, start_response):
+        data = b"Hello, World!\n"
+        start_response("200 OK", [
+            ("Content-Type", "text/plain"),
+            ("Content-Length", str(len(data)))
+        ])
+return iter([data])
