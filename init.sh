2adc928ced4973be8de5b52db92c73adf8ee5454
mln141 <ml@mail333.com>
2016-11-17 08:43:59 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/hello.py  /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
+sudo gunicorn -b 0.0.0.0:8080 hello:hello 
