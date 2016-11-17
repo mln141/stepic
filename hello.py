dde0a324aab7497d0554aa559f2d18b3829873a6
mln141 <ml@mail333.com>
2016-11-17 08:33:20 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,14 +1,5 @@
  #environ['QUERY_STRING'].replace("&", "\r\n")
 def hello(environ, start_response):
-    # бизнес-логика
-    status = '200 OK'
-    headers = [
-        ('Content-Type', 'text/plain')
-    ]
-    body = 'Hello, world!'
-    start_response(status, headers )
-    return [ body ]
-def app(environ, start_response):
         data = b"Hello, World!\n"
         start_response("200 OK", [
             ("Content-Type", "text/plain"),
