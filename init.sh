a60caccdb008972cad245a057b78cf870248e06d
mln141 <ml@mail333.com>
2016-11-20 10:45:13 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/hello.py  /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
-
-#cd web
-django-admin startproject web/ask
+cd web
+django-admin startproject ask
+cd /home/box/
 #sudo gunicorn -b 0.0.0.0:8080 web/hello:hello 
