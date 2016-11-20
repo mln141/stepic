e164b7c8619be8abcf130d2e5ce4950d3aa2b71a
mln141 <ml@mail333.com>
2016-11-20 15:42:51 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/hello.py  /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/mysql start
+#sudo /etc/init.d/mysql start
 cd web
 #django-admin startproject ask
 #python manage.py startapp qa
