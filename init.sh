fdeacdde39885492d6f7246a7b7bcbc108b90fa8
mln141 <ml@mail333.com>
2016-11-20 10:43:24 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ sudo ln -sf /home/box/web/hello.py  /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
 
-django-admin startproject web/ask
 cd web
+django-admin startproject web/ask
 #sudo gunicorn -b 0.0.0.0:8080 web/hello:hello 
